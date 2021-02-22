68c9592759274f02a54e641e1fbf7d007210c5c9
rishabhgoyal777 <71686358+rishabhgoyal777@users.noreply.github.com>
2021-02-22 15:08:30 +0530

    Delete interleave elements of 2 halves of queue.cpp

--- interleave elements of 2 halves of queue.cpp
+++
@@ -1,6 +0,0 @@
-Following are the steps to solve the problem:
-1.Push the first half elements of queue to stack.
-2.Enqueue back the stack elements.
-3.Dequeue the first half elements of the queue and enqueue them back.
-4.Again push the first half elements into the stack.
-5.Interleave the elements of queue and stack.
