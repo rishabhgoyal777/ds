91c26e2c4b3ebe98f4fda93febc02c166a5eda3f
rishabhgoyal777 <71686358+rishabhgoyal777@users.noreply.github.com>
2021-03-04 10:05:38 +0530

    Update 25b. Check if 2 nodes on same path.cpp

--- atom serial/binary trees/25b. Check if 2 nodes on same path.cpp
+++ atom serial/binary trees/25b. Check if 2 nodes on same path.cpp
@@ -1,1 +1,23 @@
-Lca
+node* lca(node* root,int a, int b, bool &flag){
+  if(!root)
+    return NULL;
+  if(root->data==a || root->data==b)
+    return root;
+  node* l=lca(root->left,a,b);
+  node* r=lca(root->right,a,b);
+  if( l && r ){
+    flag= false;
+    return root;
+  }
+  if(l)
+    return l;
+  if(r)
+    return r;
+  return NULL;
+}
+
+Bool check(node* root, int a, int b){
+  Bool flag= true;
+  lca(root,a,b,flag);
+  Return flag;
+}
