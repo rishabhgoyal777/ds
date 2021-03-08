583bd888a09e810593a3f3c93170a0b01059f71a
rishabhgoyal777 <71686358+rishabhgoyal777@users.noreply.github.com>
2021-03-08 19:45:36 +0530

    Update 12b. Vertical order.cpp

--- atom serial/binary trees/12b. Vertical order.cpp
+++ atom serial/binary trees/12b. Vertical order.cpp
@@ -1,4 +1,14 @@
-Queue pair node and int
+Queue pair node and hd
+Q.push root,0
 Map hd and vector int
 
-Inorder traversal and push element in map[hd]
+While q not empty
+Pair Temp=front
+Node t=temp.first
+Int hd=temp.second
+Map[hd].pushback t data
+If left make pair left hd-1
+If right make pair right hd+1
+
+
+// Inorder traversal and push element in map[hd]
