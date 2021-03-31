8cb88033ebd5f455bd4da50dc5c4af2780137926
rishabhgoyal777 <71686358+rishabhgoyal777@users.noreply.github.com>
2021-03-31 17:30:00 +0530

    Update and rename 23. lcs-9 sequence pattern matching.cpp to 23. lcs-9 wildcard pattern matching.cpp

---
+++ dp/23. lcs-9 wildcard pattern matching.cpp
@@ -0,0 +1,51 @@
+‘?’ – matches any single character 
+‘*’ – Matches any sequence of characters (including the empty sequence)
+
+// both text and pattern are null
+T[0][0] = true; 
+
+// pattern is null
+T[i][0] = false; 
+
+// text is null
+T[0][j] = T[0][j - 1] if pattern[j – 1] is '*'  
+
+
+bool strmatch(char str[], char pattern[], int n, int m)
+{
+    // empty pattern can only match with
+    // empty string
+    if (m == 0)
+        return (n == 0);
+
+    bool lookup[n + 1][m + 1];
+
+    // initailze lookup table to false
+    memset(lookup, false, sizeof(lookup));
+
+    // empty pattern can match with empty string
+    lookup[0][0] = true;
+
+    // Only '*' can match with empty string
+    for (int j = 1; j <= m; j++)
+        if (pattern[j - 1] == '*')
+            lookup[0][j] = lookup[0][j - 1];
+
+    // fill the table in bottom-up fashion
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+
+            if (pattern[j - 1] == '*')
+                lookup[i][j] = lookup[i][j - 1] || lookup[i - 1][j];
+ 
+            else if (pattern[j - 1] == '?'
+                     || str[i - 1] == pattern[j - 1])
+                lookup[i][j] = lookup[i - 1][j - 1];
+ 
+            else
+                lookup[i][j] = false;
+        }
+    }
+    return lookup[n][m];
+}
+ 
