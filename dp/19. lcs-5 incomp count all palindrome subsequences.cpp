133040c25e512f1ff764c3d5a5c0eaa365d4d854
rishabhgoyal777 <71686358+rishabhgoyal777@users.noreply.github.com>
2021-04-01 17:21:15 +0530

    Update 19. lcs-5 incomp count all palindrome subsequences.cpp

--- dp/19. lcs-5 incomp count all palindrome subsequences.cpp
+++ dp/19. lcs-5 incomp count all palindrome subsequences.cpp
@@ -1,38 +1,32 @@
 INCOMPLETE
 ========== https://leetcode.com/problems/count-different-palindromic-subsequences/submissions/
 
-class Solution {
-public:
-    int lcs(int x, int y, string s1, string s2){
+int countPS(string str)
+{
+    int N = str.length();
 
-    int t[x+1][y+1];
-    for(int i=0;i<x+1;i++){
-        for(int j=0;j<y+1;j++){
-            if(i==0 || j==0)
-                t[i][j]=0;
-        }
-    }
-    for(int i=0;i<x+1;i++){
-        for(int j=0;j<y+1;j++){
-            if(i==j)
-                t[i][j]=1;
-        }
-    }
-    for(int i=1;i<x+1;i++){
-        for(int j=1;j<y+1;j++){
-            if(s1[i-1]==s2[j-1])
-                t[i][j]= 1+t[i][j-1]+t[i-1][j];
+    // create a 2D array to store the count of palindromic
+    // subsequence
+    int cps[N + 1][N + 1];
+
+    memset(cps, 0, sizeof(cps));
+
+    // palindromic subsequence of length 1
+    for (int i = 0; i < N; i++)
+        cps[i][i] = 1;
+ 
+    // check subsequence of length L is palindrome or not
+    for (int L = 2; L <= N; L++) {
+        for (int i = 0; i <= N-L; i++) {
+            int k = L + i - 1;
+            if (str[i] == str[k])
+                cps[i][k]
+                    = cps[i][k - 1] + cps[i + 1][k] + 1;
             else
-                t[i][j]= t[i-1][j]+t[i][j-1] - t[i-1][j-1];
+                cps[i][k] = cps[i][k - 1] + cps[i + 1][k]
+                            - cps[i + 1][k - 1];
         }
     }
-    return t[x][y];
+    // return total palindromic subsequence
+    return cps[0][N - 1];
 }
-
-int countPalindromicSubsequences(string s){
-  int x=s.length();
-  string a=s;
-  reverse(a.begin(), a.end());
-  return lcs(x,x,s,a);
-  }
-};
