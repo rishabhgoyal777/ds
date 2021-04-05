0e30a6360cf9d002056fe685bfa2edb66f3097ab
rishabhgoyal777 <71686358+rishabhgoyal777@users.noreply.github.com>
2021-04-06 02:38:26 +0530

    Update 11. unkn-2 coin change problem max ways.cpp

--- dp/11. unkn-2 coin change problem max ways.cpp
+++ dp/11. unkn-2 coin change problem max ways.cpp
@@ -7,7 +7,7 @@ class Solution
     long long int count( int arr[], int m, int n )
     {
 
-        long long int t[m+1][n+1];
+        long long int t[m+1][n+1];   // m is size of array // n is required change 
         for(int i=0;i<m+1;i++)
             t[i][0]=1;              // sum 0 bnana arr mei jitne mrji coin ho ways=1
         for(int j=1;j<n+1;j++)
