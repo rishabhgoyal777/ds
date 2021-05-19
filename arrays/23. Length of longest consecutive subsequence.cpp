f12b98963a80d6b2e8961f93ab143620312a2fab
rishabhgoyal777 <71686358+rishabhgoyal777@users.noreply.github.com>
2021-05-19 22:55:03 +0530

    Update 23. Length of longest consecutive subsequence.cpp

--- arrays/23. Length of longest consecutive subsequence.cpp
+++ arrays/23. Length of longest consecutive subsequence.cpp
@@ -50,34 +50,31 @@ O(n)
 
 int findLongestConseqSubseq(int arr[], int n)
 {
-    int ans = 0, count = 0;
+    unordered_set<int> S;
+    int ans = 0;
  
-    // sort the array
-    sort(arr, arr + n);
+    // Hash all the array elements
+    for (int i = 0; i < n; i++)
+        S.insert(arr[i]);
  
-    vector<int> v;
-    v.push_back(arr[0]);
- 
-    //insert repeated elements only once in the vector
-    for (int i = 1; i < n; i++)
+    // check each possible sequence from
+    // the start then update optimal length
+    for (int i = 0; i < n; i++)
     {
-        if (arr[i] != arr[i - 1])
-            v.push_back(arr[i]);
-    }
-    // find the maximum length
-    // by traversing the array
-    for (int i = 0; i < v.size(); i++)
-    {   
-        // Check if the current element is equal
-        // to previous element +1
-        if (i > 0 && v[i] == v[i - 1] + 1)
-            count++;
-        // reset the count
-        else
-            count = 1;
+        // if current element is the starting
+        // element of a sequence
+        if (S.find(arr[i] - 1) == S.end())
+        {
+            // Then check for next elements
+            // in the sequence
+            int j = arr[i];
+            while (S.find(j) != S.end())
+                j++;
  
-        // update the maximum
-        ans = max(ans, count);
+            // update  optimal length if
+            // this length is more
+            ans = max(ans, j - arr[i]);
+        }
     }
     return ans;
 }
