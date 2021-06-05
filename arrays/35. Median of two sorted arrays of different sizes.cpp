cb81aa5f722933cb0a2e715b1d6cd4a26ab5c92a
rishabhgoyal777 <71686358+rishabhgoyal777@users.noreply.github.com>
2021-06-05 12:56:20 +0530

    Update 35. Median of two sorted arrays of different sizes.cpp

--- arrays/35. Median of two sorted arrays of different sizes.cpp
+++ arrays/35. Median of two sorted arrays of different sizes.cpp
@@ -64,3 +64,45 @@ int getMedian(int ar1[], int ar2[], int n, int m)
         return (m1 + m2)/2;
     }
 }
+
+
+Time Complexity => O(log(min(n1,n2)))  (striver)
+================================
+  
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        if( nums1.size() > nums2.size() )
+            return findMedianSortedArrays(nums2,nums1);  // we are keeping n1 the smaller array. so that binary search is on smaller length
+        int n1=nums1.size();
+        int n2=nums2.size();
+        int low=0;
+        int high=n1;
+        
+        
+        while(low<=high){
+            int cut1=(low+high)/2;    // element in 1st half =>  0 to cut1-1 from nums1 and 0 to cut2 -1 from nums2 similarly in second half
+            int cut2=(n1+n2+1)/2 - cut1;
+            
+            int l1 = cut1==0 ? INT_MIN : nums1[cut1-1]; 
+            int l2 = cut2==0 ? INT_MIN : nums2[cut2-1]; 
+            int r1 = cut1==n1 ? INT_MAX: nums1[cut1]; 
+            int r2 = cut2==n2 ? INT_MAX: nums2[cut2] ; 
+            
+            if(l1<=r2 && l2 <= r1){
+                if((n1+n2) % 2 ==0)
+                    return((max(l1,l2) + min(r1,r2))/2.0); 
+                else
+                    return max(l1,l2);
+            }
+            
+            else if(l1 > r2){
+                high = cut1-1;
+            }
+            else{
+                low = cut1+1;
+            }
+        }
+        return 0;           
+    }
+};
