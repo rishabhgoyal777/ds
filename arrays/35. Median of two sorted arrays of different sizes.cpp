74704cbc3fb798a3d446829c25d6b9aabedb0afe
rishabhgoyal777 <71686358+rishabhgoyal777@users.noreply.github.com>
2021-06-05 13:44:29 +0530

    Update 35. Median of two sorted arrays of different sizes.cpp

--- arrays/35. Median of two sorted arrays of different sizes.cpp
+++ arrays/35. Median of two sorted arrays of different sizes.cpp
@@ -82,24 +82,26 @@ public:
         
         while(low<=high){
             int cut1=(low+high)/2;    // element in 1st half =>  0 to cut1-1 from nums1 and 0 to cut2 -1 from nums2 similarly in second half
-            int cut2=(n1+n2+1)/2 - cut1;
+            int cut2=(n1+n2+1)/2 - cut1;  //n1+n2+1/2 gives middle for both odd and even
             
-            int l1 = cut1==0 ? INT_MIN : nums1[cut1-1]; 
-            int l2 = cut2==0 ? INT_MIN : nums2[cut2-1]; 
-            int r1 = cut1==n1 ? INT_MAX: nums1[cut1]; 
-            int r2 = cut2==n2 ? INT_MAX: nums2[cut2] ; 
+            int l1 = cut1==0 ? INT_MIN : nums1[cut1-1]; //max element in first half from nums1[0....cut1-1]
+            int l2 = cut2==0 ? INT_MIN : nums2[cut2-1]; //max element in first half from nums2[0.....cut2-1]
+            int r1 = cut1==n1 ? INT_MAX: nums1[cut1];   //min element in second half from nums1[cut1.....n1-1]
+            int r2 = cut2==n2 ? INT_MAX: nums2[cut2] ;  //min element in second half from nums2[cut2.....n2-1]
             
-            if(l1<=r2 && l2 <= r1){
+            if(l1<=r2 && l2 <= r1){  //consition for valid halves
                 if((n1+n2) % 2 ==0)
-                    return((max(l1,l2) + min(r1,r2))/2.0); 
+                    return((max(l1,l2) + min(r1,r2))/2.0);  
                 else
                     return max(l1,l2);
             }
             
             else if(l1 > r2){
+              //we need to decrease l1, that is partition should be done between low and cut1. therefore
                 high = cut1-1;
             }
             else{
+              //we need to increase r1, taht is partition should be done between cut1 and high
                 low = cut1+1;
             }
         }
