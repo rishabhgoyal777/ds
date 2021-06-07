bd5e872c7e1618e38119ac0a4c0839398eb7e2db
rishabhgoyal777 <71686358+rishabhgoyal777@users.noreply.github.com>
2021-06-07 16:06:51 +0530

    Create common elements in all rows.cpp

---
+++ matrix/common elements in all rows.cpp
@@ -0,0 +1,18 @@
+vector<int> printCommonElements(int mat[n][m]){
+  
+  unordered_map<int, int> mp;
+  for(int j=0;j<m;j++){ //storing first row
+    mp[mat[0][j]]=1; 
+  }
+  
+  for(int i=1;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(mp[mat[i][j]]==i)  //dulpicate in row will not be taken since it will be greater than i after the first occurence
+        mp[mat[i][j]]++;
+        
+        if(i==n-1 && mp[mat[i][j]]==n) //if it is last row.
+          v.push_back(mat[i][j]);
+    }
+  }
+  return v;
+}
