711d8a3978018ed625e88c31b1dba994c6f345da
rishabhgoyal777 <71686358+rishabhgoyal777@users.noreply.github.com>
2021-06-11 19:15:01 +0530

    Update 6. longest palindrome substring.cpp

--- string/6. longest palindrome substring.cpp
+++ string/6. longest palindrome substring.cpp
@@ -5,6 +5,10 @@ int longestPalSubstr(char* str)
     int len = strlen(str);
  
     int low, high;
+    
+    // One by one consider every
+    // character as center point of
+    // even and length palindromes
     for (int i = 1; i < len; ++i) {
         // Find the longest even length palindrome
         // with center points as i-1 and i.
