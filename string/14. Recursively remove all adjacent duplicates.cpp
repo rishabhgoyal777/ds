74cf5739e16924f0c723c064904ff7c8c9edf3cf
rishabhgoyal777 <71686358+rishabhgoyal777@users.noreply.github.com>
2021-06-15 21:08:13 +0530

    Create 14. Recursively remove all adjacent duplicates.cpp

---
+++ string/14. Recursively remove all adjacent duplicates.cpp
@@ -0,0 +1,16 @@
+Given a string S delete the characters which are appearing more than once consecutively.
+
+  
+    string removeConsecutiveCharacter(string s)
+    {
+        if(s.length() < 2)
+            return s;
+        int j=0;  
+        for(int i=1;i<s.length();i++){ //if equal i incremented
+            if(s[i]!=s[j]){  //if not equal j incremented and changed with next different element
+               j++;
+               s[j]=s[i];
+            }
+        }
+        return s.substr(0,j+1);
+    }
