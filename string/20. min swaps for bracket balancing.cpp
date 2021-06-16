680fe1a0f96cf112a3d1247c724d3983d5206102
rishabhgoyal777 <71686358+rishabhgoyal777@users.noreply.github.com>
2021-06-16 12:43:47 +0530

    Update 20. min swaps for bracket balancing.cpp

--- string/20. min swaps for bracket balancing.cpp
+++ string/20. min swaps for bracket balancing.cpp
@@ -1,3 +1,8 @@
+You are given a string S of 2N characters consisting of N ‘[‘ brackets and N ‘]’ brackets. A string is considered balanced if 
+it can be represented in the for S2[S1] where S1 and S2 are balanced strings. We can make an unbalanced string balanced by swapping adjacent characters. 
+Calculate the minimum number of swaps necessary to make a string balanced.
+Note - Strings S1 and S2 can be empty.
+
 int minimumNumberOfSwaps(string s){
          int n=s.length();
          int ans=0;
@@ -10,7 +15,7 @@ int minimumNumberOfSwaps(string s){
                  open++;
                  if(imbalance > 0){
                      ans+=imbalance;
-                     imbalance--;
+                     imbalance--; //ye open ne ek imbalance thik krdiya swap krke
                  }
              }
              else if(s[i]==']'){
