0c22e6d04ff3f3f2c721b3e48ece5a346d321c16
rishabhgoyal777 <71686358+rishabhgoyal777@users.noreply.github.com>
2021-06-22 15:43:25 +0530

    Create 12. heap sort.cpp

---
+++ heap/12. heap sort.cpp
@@ -0,0 +1,36 @@
+void heapify(int arr[], int i, int n){
+  if(i>=n)
+     return;
+  
+  int parent = i;
+  int lchild = 2*i+1; 
+  int rchild = 2*i+2;
+  
+  if(lchild < n && arr[lchild] > arr[parent])
+     parent = lchild;
+  if(rchild < n && arr[rchild] > arr[parent])
+     parent = rchild;
+  
+  if(parent != i){
+    swap(arr[i], arr[parent]);
+    heapify(arr,parent,n);
+  }
+}
+
+
+void heapsort(int arr[], int n){
+  int i=n/2 -1;
+  
+  //heapify=> maxheap. Arr[0] will be the greatest ele
+  for(i;i>=0;i--){
+    heapify(arr,i,n);
+  }
+  
+  //sorting
+//   starting from last index. swapping greatest ele to last index and heapifying remaining arr of size i or n-1 at index 0. 
+  for(i=n-1;i>0;i--){
+    swap(arr[i], arr[0]);
+    heapify(arr,0,i):
+  } 
+}
+    
