2969d70b1255f1e284eb38277b15ee665138b210
rishabhgoyal777 <71686358+rishabhgoyal777@users.noreply.github.com>
2021-06-26 13:52:55 +0530

    Update 16. sort aray where adjacent differ by atmost k.cpp

--- searching and sorting/16. sort aray where adjacent differ by atmost k.cpp
+++ searching and sorting/16. sort aray where adjacent differ by atmost k.cpp
@@ -3,8 +3,9 @@ int search(int arr[], int n, int x, int k){
   while(i<n){
     if(arr[i]==x)
        return i;
-    int diff=abs(arr[i]-x);
-    i+= max(1,diff/k);
+    //if element not equal
+    int diff=abs(arr[i]-x); //find diff bw current element and required element
+    i+= max(1,diff/k); // skip diff/k index. and if required element is in k range from current element diff/k wil be 0. so move max of 1 and diff/k.
   }
   return -1;
 }
