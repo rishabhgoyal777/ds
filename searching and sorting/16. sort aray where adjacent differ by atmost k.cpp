9459668414b558e6bd5b377c814545734a22da4e
rishabhgoyal777 <71686358+rishabhgoyal777@users.noreply.github.com>
2021-06-26 13:37:16 +0530

    Create 16. sort aray where adjacent differ by atmost k.cpp

---
+++ searching and sorting/16. sort aray where adjacent differ by atmost k.cpp
@@ -0,0 +1,11 @@
+int search(int arr[], int n, int x, int k){
+  int i=0;
+  while(i<n){
+    if(arr[i]==x)
+       return i;
+    int diff=abs(arr[i]-x);
+    i+= max(1,diff/k);
+  }
+  return -1;
+}
+    
