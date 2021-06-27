38fca43c8fdcde4cfc835be99f98a8da38668f4d
rishabhgoyal777 <71686358+rishabhgoyal777@users.noreply.github.com>
2021-06-28 02:28:59 +0530

    Update 23. Smallest number with at least n trailing zeroes in factorial.cpp

--- searching and sorting/23. Smallest number with at least n trailing zeroes in factorial.cpp
+++ searching and sorting/23. Smallest number with at least n trailing zeroes in factorial.cpp
@@ -1,7 +1,12 @@
-bool check(int n, int mid){
+// TO CHECK TRAILING ZEROS IN FACTORIAL WE CHECK NO OF 5 IN DIVISORS. BEC NO OF 2's WILL BE HIGHER ONLY
+EXAMPLE => 10! = 10*9*8*7*6*5*4*3*2*1 => NO OF 5's = 2 => 10/2 THERFORE N/2
+            BUT FOR NUMBERS LIKE 25, 125 THERE ARE EXTRA 5's IN ANSWER
+            SO FORMULAE FOR TOTAL 5's IN N = floor(N/5) + floor(N/5^2) + floor(N/5^3) + ... UNTIL TERM BECOMES ZERO
+
+       bool check(int n, int mid){
             int d=5;
             int ans=0;
-            while(mid/d){
+            while(mid/d){   
                 ans+=mid/d;
                 d=d*5;
             }
@@ -17,9 +22,9 @@ bool check(int n, int mid){
             int ans=0;
             
             while(l<=h){
-                int mid=(l+h)/2;
+                int mid=(l+h)/2; // posible number
                 
-                if(check(n, mid)){
+                if(check(n, mid)){ // if this mid has more than equal to n zeros than check in lower numbers. all higher will also have.
                     ans=mid;
                     h=mid-1;
                 }
