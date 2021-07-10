5f93766d9d4be03f552e660f0fb2170d91c5a979
rishabhgoyal777 <71686358+rishabhgoyal777@users.noreply.github.com>
2021-07-10 17:00:32 +0530

    Update 16. choose and swap.cpp

--- greedy/16. choose and swap.cpp
+++ greedy/16. choose and swap.cpp
@@ -2,14 +2,14 @@ string chooseandswap(string a){
     set<char> s;
     for(int i=0;i<a.length();i++){
         s.insert(a[i]);
-    }
+    } //set. so will be in increasing order
 
     for(int i=0;i<a.length();i++){
-        s.erase(a[i]);
+        s.erase(a[i]); //now this wont be used afterwards so erase it.
         if(s.empty())
             break;
-        char temp=*s.begin();
-        if(temp < a[i]){
+        char temp=*s.begin();  // smallest from the set
+        if(temp < a[i]){ // if smaller than current. swap all their occurences
             char temp2=a[i];
             for(int j=0;j<a.length();j++){
                 if(a[j]==temp2) a[j]=temp;
